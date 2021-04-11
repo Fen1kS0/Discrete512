1f03de7833562ddae8bcce098b5c46c16e5d34c1
Alex <60792527+Fen1kS0@users.noreply.github.com>
2021-04-11 19:49:12 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,20 +9,20 @@ namespace Permutations512
     class Program
     {
         /// <summary>
-        /// Кол-во различных слов, которые можно получить перестановкой букв слова a, подходящих под условие
+        /// Кол-во различных слов, которые можно получить перестановкой букв слова alpha, подходящих под условие
         /// </summary>
-        private static int Count = 0;
+        private static int _count;
         
         // Для 5 варианта (вынес ради экономии памяти)
-        private static readonly string vowels = "аеёиоуыэюя";
-        private static readonly string consonants = "бвгджзйклмнпрстфхцчшщьъ";
-        private static readonly string pattern = $@"\w*([{vowels}][{consonants}][{consonants}][{vowels}])\w*";
+        private const string Vowels = "аеёиоуыэюя";
+        private const string Consonants = "бвгджзйклмнпрстфхцчшщьъ";
+        private static readonly string Pattern = $@"\w*([{Vowels}][{Consonants}][{Consonants}][{Vowels}])\w*";
         // ============================================
         
         static void Main(string[] args)
         {
             string word = "пастух"; // Меняем на нужное нам слово
-            int len = word.Length;
+            int maxLength = word.Length;
 
             var group = word.GroupBy(g => g);
             
@@ -39,26 +39,26 @@ namespace Permutations512
             TimeSpan ts;
 
             stopWatch.Start();
-            GetCountPermutations("", ref letters, ref countLetters, ref len);
+            GetCountPermutations("", letters, countLetters, ref maxLength);
             Console.WriteLine($"Кол-во перестановок подходящее под условие = {Count}");
             stopWatch.Stop();
+            
             ts = stopWatch.Elapsed;
             Console.WriteLine("Время выполнения = {0:00}:{1:00}.{2:00}",ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            
             Console.ReadLine();
         }
         
         /// <summary>
         /// Тут без полного понимания лучше ничего не трогать
         /// </summary>
-        static void GetCountPermutations(string per, ref char[] letters, ref int[] countLetters, ref int maxLength)
+        static void GetCountPermutations(string per, char[] letters, int[] countLetters, ref int maxLength)
         {
             if (per.Length == maxLength)
             {
                 if (Test(per))
                 {
                     Count++;
-                    // Console.WriteLine(per); // Для вывода перестановки на консоль (Сильно влияет на быстродействие)
+                    Console.WriteLine(per); // Для вывода перестановки на консоль (Сильно влияет на быстродействие)
                 }
                 
                 return;
@@ -69,7 +69,7 @@ namespace Permutations512
                 if (countLetters[i] > 0)
                 {
                     countLetters[i]--;
-                    GetCountPermutations(per + letters[i], ref letters, ref countLetters, ref maxLength);
+                    GetCountPermutations(per + letters[i], letters, countLetters, ref maxLength);
                     countLetters[i]++;
                 }
             }
@@ -80,10 +80,10 @@ namespace Permutations512
         /// Метод принимает строку и проверяет её на выполнение условия
         /// </summary>
         /// <param name="s">Строка для проверки</param>
-        /// <returns>true или false</returns>
+        /// <returns>true если слово проходит проверку,иначе false</returns>
         static bool Test(string s)
         {
-            return Regex.IsMatch(s, pattern, RegexOptions.IgnoreCase);
+            return Regex.IsMatch(s, Pattern, RegexOptions.IgnoreCase);
         }
     }
 }
